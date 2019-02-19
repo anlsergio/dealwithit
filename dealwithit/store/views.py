701b0a60ee4e87d2d1dd00fe6ee428bec29ae69d
Anderson Luis Sergio <anderson.als04@gmail.com>
2019-02-19 08:14:17 -0300

    creates a grid system of cards in order to list the products beautifuly

--- dealwithit/store/views.py
+++ dealwithit/store/views.py
@@ -1,10 +1,64 @@
-from django.http import HttpResponse
 from django.shortcuts import render
+
+
+products = [
+    {
+        'name': 'Product 1',
+        'description': 'A great product',
+        'seller': 'John Snow',
+        'price': 120.90,
+        'date_posted': 'August 27, 2018',
+        'image': 'https://support.apple.com/library/content/dam/edam/applecare/images/en_US/il/ios12-product-lockup-callout.png'
+    },
+    {
+        'name': 'Product 2',
+        'description': 'Another great product',
+        'seller': 'Daenerys Targerian',
+        'price': 400.20,
+        'date_posted': 'August 23, 2018',
+        'image': 'https://support.apple.com/library/content/dam/edam/applecare/images/en_US/il/ios12-product-lockup-callout.png'
+    },
+    {
+        'name': 'Product 3',
+        'description': 'Not so good at all',
+        'seller': 'Twin Lannister',
+        'price': 99.50,
+        'date_posted': 'February 12, 2019',
+        'image': 'https://support.apple.com/library/content/dam/edam/applecare/images/en_US/il/ios12-product-lockup-callout.png'
+    },
+    {
+        'name': 'Product 1',
+        'description': 'A great product',
+        'seller': 'John Snow',
+        'price': 120.90,
+        'date_posted': 'August 27, 2018',
+        'image': 'https://support.apple.com/library/content/dam/edam/applecare/images/en_US/il/ios12-product-lockup-callout.png'
+    },
+    {
+        'name': 'Product 2',
+        'description': 'Another great product',
+        'seller': 'Daenerys Targerian',
+        'price': 400.20,
+        'date_posted': 'August 23, 2018',
+        'image': 'https://support.apple.com/library/content/dam/edam/applecare/images/en_US/il/ios12-product-lockup-callout.png'
+    },
+    {
+        'name': 'Product 3',
+        'description': 'Not so good at all',
+        'seller': 'Twin Lannister',
+        'price': 99.50,
+        'date_posted': 'February 12, 2019',
+        'image': 'https://support.apple.com/library/content/dam/edam/applecare/images/en_US/il/ios12-product-lockup-callout.png'
+    }
+]
 
 
 # Create your views here.
 def home(request):
-    return HttpResponse('<h1>Store home</h1>')
+    context = {
+        'products': products
+    }
+    return render(request, 'store/home.html', context)
 
 def about(request):
-    return HttpResponse('<h1>About</h1>')
+    return render(request, 'store/about.html', {'title': 'About'})
