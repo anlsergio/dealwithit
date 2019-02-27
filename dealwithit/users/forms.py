6bae2aba1b493e51bbbabec7d138b1cdcf2173a4
Anderson Luis Sergio <anderson.als04@gmail.com>
2019-02-27 18:47:16 -0300

    This adds seller's contact info to the product detail page

--- dealwithit/users/forms.py
+++ dealwithit/users/forms.py
@@ -34,7 +34,7 @@ class UserUpdateForm(forms.ModelForm):
 
 class ProfileUpdateForm(forms.ModelForm):
     phone_number = PhoneNumberField(label='Phone Number (+9999999999 up to 15 digits)')
-    phone_number_is_public = forms.BooleanField(label="Show my phone number")
+    phone_number_is_public = forms.BooleanField(label="Show my phone number", required=False)
     class Meta:
         model = Profile
         fields = [
