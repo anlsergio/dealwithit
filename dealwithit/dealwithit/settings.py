ef0decab8c4a487f86833bfe904038058e7310f9
Anderson Luis Sergio <anderson.als04@gmail.com>
2019-03-01 19:58:22 -0300

    Using a remote MariaDB connection instead of Sqlite

--- dealwithit/dealwithit/settings.py
+++ dealwithit/dealwithit/settings.py
@@ -79,10 +79,21 @@ WSGI_APPLICATION = 'dealwithit.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'dealwithit', # database name
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWD'),
+        'HOST': config('HOST'),
+        'PORT': config('PORT'),
     }
 }
 
