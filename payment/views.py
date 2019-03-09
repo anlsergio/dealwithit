4f6cae270503ec3389bfd8990719e04078f303a8
Anderson Luis Sergio <anderson.als04@gmail.com>
2019-03-09 18:48:48 -0300

    Enabling Stripe to handle dynamic amount chosen by the user

--- payment/views.py
+++ payment/views.py
@@ -1,8 +1,9 @@
+import decimal
+
 import stripe
 from django.conf import settings
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
-
 
 
 @login_required
@@ -11,12 +12,25 @@ def payment(request):
 
 @login_required
 def charge(request):
+
+    chosen_amount = request.POST.get("dropdown")
+
     if request.method == 'POST':
-        charge = stripe.Charge.create(
-            amount=500,
-            currency='usd',
-            description='Dealwithit store charge',
-            source=request.POST['stripeToken'],
-            api_key = settings.STRIPE_SECRET_KEY
-        )
+        try:
+            charge = stripe.Charge.create(
+                amount=chosen_amount,
+                currency='usd',
+                description='Dealwithit store charge',
+                source=request.POST['stripeToken'],
+                api_key = settings.STRIPE_SECRET_KEY
+            )
+            # Captures the instance object of the current logged user
+            # And adds the chargeable ammount of credit
+            # Stripe uses cent type ammount, so that's why we should divide it by 100 to convert it to decimal
+            # Which is a more readable ammount type
+            current_user = request.user
+            current_user.profile.credit += decimal.Decimal(int(chosen_amount)/100)
+            current_user.save()
+        except stripe.error.CardError as ce:
+            return False, ce
         return render(request, 'payment/charge.html')
